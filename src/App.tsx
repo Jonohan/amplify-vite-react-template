d31030cb45fd835ac41e0edc5464a174050c893b
Jonohan <wanghhjohn@outlook.com>
2024-05-14 10:32:14 -0700

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       {({ signOut, user }) => (
         /* for login */
       <main>
-        <h1>My todos</h1>
+        <h1>{user?.signInDetails?.loginId}'s todos</h1>
         <button onClick={createTodo}>+ new</button>
         <ul>
           {todos.map(todo => <li
